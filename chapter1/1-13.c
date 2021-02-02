93ef6e3060fe639130083ae60d7a74583ded7b8f
hotNoodleSoup <ak1925@naver.com>
2021-02-03 00:27:42 +0900

    2/3

---
+++ chapter1/1-13.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define MAXWORD 10
+#define IN 1
+#define OUT 0
+
+main()
+{
+	int c;
+	int wc[MAXWORD] = {0};
+	int count = 0;
+	char state = OUT;
+
+	while((c = getchar()) != EOF) {
+		if (c >= 'a' && c <= 'z' || c>= 'A' && c<= 'Z') {
+			state = IN;
+			++count;
+		}
+		else if (state == IN) {
+			state = OUT;
+			++wc[count - 1];
+			count = 0;
+		}
+	}
+	
+	int i, j;
+
+	for (i = 35; i >= 1; --i) {
+		printf("%2d |", i);
+		for (j = 0; j < 10; ++j) {
+			if (wc[j] == i) {
+				printf(" | ");
+				--wc[j];
+			}
+			else
+				printf("   ");
+		}
+		printf("\n");
+	}
+
+	printf("   " "*" "----------" "----------" "----------" "\n");
+	printf("   ");
+	for (i = 1; i <=10; ++i)
+		printf( "%3d", i);
+
+	printf("\n");
+}
