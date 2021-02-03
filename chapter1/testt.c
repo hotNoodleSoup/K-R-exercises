9748dccbccad2e37e385a82a2bb49e8280d3bd3f
hotNoodleSoup <ak1925@naver.com>
2021-02-03 21:44:32 +0900

    2/3

--- chapter1/testt.c
+++ chapter1/testt.c
@@ -1,29 +1,59 @@
+#include <ctype.h>
+#include <limits.h>
 #include <stdio.h>
-int main(){
 
-    int userInput;
-    int arrayStuff[92];
-    int i, j;
+/*  print a graph of the frequencies of different characters */
 
-    for(i = 0; i < 92; ++i){
-        arrayStuff[i] = 0;
+#define GRAPH_HEIGHT 20
+#define AA (3 * UCHAR_MAX/4) 
+#define BB (4 * UCHAR_MAX/4)
+
+void count_chars(FILE *in, unsigned int *frequencies)
+{
+    int c;
+    while ((c = fgetc(in)) != EOF) {
+        ++frequencies[(unsigned char)c];
     }
+}
 
-    while((userInput = getchar()) != '\n'){
-        if(userInput >= 30 && userInput <= 122){
-            if(userInput != '\n'){
-                ++arrayStuff[(userInput-30)];
-            }
-
-            if(userInput == '\n'){
-                break;
-            }
+void print_graph(FILE *out, const unsigned int *frequencies)
+{
+    /* start at 1 to avoid division by 0 later */
+    unsigned int max = 1;
+    for (unsigned int i = 0;  i <= UCHAR_MAX;  ++i) {
+        if (frequencies[i] > max) {
+            max = frequencies[i];
         }
     }
 
-    printf("Case\t|\tOccurances\n");
-
-    for(i = 0; i < 92; ++i){
-        printf("%c\t|\t%d\n", (i+30), arrayStuff[i]);
+    /* print characters on a graph */
+    for (unsigned int i = GRAPH_HEIGHT;  i > 0;  --i) {
+        fprintf(out, "%2d |", i);
+        for (int c = AA;  c <= BB;  ++c) {
+         
+            char bar = frequencies[c] >= i * max / GRAPH_HEIGHT ? '|' : ' ';
+            fprintf(out, " %c ", bar);
+        }
+        fputs("\n", out);
     }
+
+    /* print the base line */
+    fputs("   +", out);
+    for (int c = AA;  c <= BB;  ++c) {
+       
+        fputs("---", out);
+    }
+    fputs("\n   ", out);
+    for (int c = AA;  c <= BB;  ++c) {
+        
+        fprintf(stdout, "%3c", c);
+    }
+    fputs("\n", out);
+}
+
+int main(void)
+{
+    unsigned int af[UCHAR_MAX+1] = {0};
+    count_chars(stdin, af);
+    print_graph(stdout, af);
 }
